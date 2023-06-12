cfb0efe7950d41bb70699b8b12efb237cc20e295
Brandon Riggs <aceriggs7@gmail.com>
2023-06-12 16:57:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let homeTeam = 5
+let awayTeam = 3
+
+function homeNumber() {
+  homeTeam++;
+  return homeTeam;
+}
+
+function awayNumber(){
+  awayTeam++;
+  return awayTeam;
+}
+function draw(){
+  document.getElementById("homeTeam")
+}
